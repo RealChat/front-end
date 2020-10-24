ac707def48eda5c68798a20e26ccf6fb5fdb51b9
SwatiMishra8517 <57552806+SwatiMishra8517@users.noreply.github.com>
2020-10-24 21:32:25 +0530

    func signup resolved conflicts

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,84 +1,82 @@
-import React, { Component } from 'react'
+import React, { Component, useState, useContext } from 'react'
 import './CSS/login.css'
 import client from '../api/client'
 import Context from '../Context'
 import { Link } from "react-router-dom";
-export class Signup extends Component {
-    state = {
-        name:"",
-        email:"",
-        username:"",
-        password:"",
-        error:"",
-        success:""
-    }
-    handleSubmit = (e)=>{
+
+function Signup() {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [username, setUsername] = useState("");
+    const [error, setError] = useState("");
+    const [success, setSuccess] = useState("");
+
+    const handleSubmit = (e) => {
         e.preventDefault()
 
-        const { name,email,password,username } = this.state
-        client.post("/auth/signup",{
+        client.post("/auth/signup", {
             name,
             email,
             password,
             username
         })
-        .then(res=>{
-            if(res.error){
-                this.setState({error:res.error})
-            }
-            else{
-                this.setState({success:res.message})
+            .then(res => {
+                if (res.error) {
+                    setError(res.error)
+                }
+                else {
+                    setSuccess("you are registered successfully")
 
-            }
-        })
+                }
+            })
     }
-    render() {
-        return (
-            <div className="login">
-                <form onSubmit={this.handleSubmit}>
-                    <h1>Signup</h1>
-                    <p style={{ color: "red" }}>{this.state.error}</p>
-                    <p style={{ color: "green" }}>{this.state.success}</p>
-                    <input
-                        onChange={(e) =>
-                            this.setState({ name: e.target.value })
-                        }
-                        type="text"
-                        name="name"
-                        placeholder="name"
-                    />
-                    <input
-                        onChange={(e) =>
-                            this.setState({ username: e.target.value })
-                        }
-                        type="text"
-                        name="username"
-                        placeholder="username"
-                    />
-                    <input
-                        onChange={(e) =>
-                            this.setState({ email: e.target.value })
-                        }
-                        type="email"
-                        name="email"
-                        placeholder="email"
-                    />
-                    <input
-                        onChange={(e) =>
-                            this.setState({ password: e.target.value })
-                        }
-                        type="password"
-                        name="pwd"
-                        placeholder="password"
-                    />
-                    <input type="submit" value="signup" />
-                    <p>
-                            If already registered then <Link to="/login">Login</Link>
-                        </p>
-                </form>
-            </div>
-        );
-    }
+    return (
+        <div className="login">
+            <form onSubmit={handleSubmit}>
+                <h1>Signup</h1>
+                <p style={{ color: "red" }}>{error}</p>
+                <p style={{ color: "green" }}>{success}</p>
+                <input
+                    onChange={(e) =>
+                        setName(e.target.value)
+                    }
+                    type="text"
+                    name="name"
+                    placeholder="name"
+                />
+                <input
+                    onChange={(e) =>
+                        setUsername(e.target.value)
+                    }
+                    type="text"
+                    placeholder="username"
+                />
+                <input
+                    onChange={(e) =>
+                        setEmail( e.target.value)
+                    }
+                    type="email"
+                    name="email"
+                    placeholder="email"
+                />
+                <input
+                    onChange={(e) =>
+                        setPassword(e.target.value)
+                    }
+                    type="password"
+                    name="pwd"
+                    placeholder="password"
+                />
+                <input type="submit" value="signup" />
+                <p>
+                    If already registered then <Link to="/login">Login</Link>
+                </p>
+            </form>
+        </div>
+    );
 }
+
 
 export default Signup
